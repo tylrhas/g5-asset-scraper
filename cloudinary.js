02d80577b40041deed2a173b1b3803c4956e0cc4
David Miller <david.miller@getg5.com>
2020-10-05 10:56:13 -0700

    removed semis

--- cloudinary.js
+++ cloudinary.js
@@ -1,32 +1,32 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2
 
 const {
   CLOUDINARY_NAME,
   CLOUDINARY_KEY,
   CLOUDINARY_SECRET
-} = process.env;
+} = process.env
 
 cloudinary.config({
   cloud_name: CLOUDINARY_NAME,
   api_key: parseInt(CLOUDINARY_KEY),
   api_secret: CLOUDINARY_SECRET
-});
+})
 
 module.exports = {
   upload
-};
+}
 
 function upload (url, location, page) {
-  console.log(`Uploading from ${url}`);
+  console.log(`Uploading from ${url}`)
   return new Promise((res, rej) => {
     cloudinary.uploader.upload(url, { folder: `${location}/${page}` }, function (err, response) {
       if (!err) {
-        res(response);
+        res(response)
       } else {
-        rej(err);
+        rej(err)
       }
-    });
-  });
+    })
+  })
 }
 
 function getImage (id) {}
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json({ limit: '1000kb' }));
-const Scraper = require('./scraper');
-const { upload } = require('./cloudinary');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json({ limit: '1000kb' }))
+const Scraper = require('./scraper')
+const { upload } = require('./cloudinary')
 
 app.post('/', async (req, res) => {
-  const { body } = req;
-  const scraper = new Scraper(body);
-  await scraper.run();
-  res.send('Hello');
-});
+  const { body } = req
+  const scraper = new Scraper(body)
+  await scraper.run()
+  res.send('Hello')
+})
 
 app.post('/upload', async (req, res) => {
-  const { imgSrc } = req.body;
-  const cloudUrl = await upload(imgSrc);
-  res.json(cloudUrl);
-});
+  const { imgSrc } = req.body
+  const cloudUrl = await upload(imgSrc)
+  res.json(cloudUrl)
+})
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`)
+})
