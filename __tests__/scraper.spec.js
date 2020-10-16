26ce19da16c520c32618f919b9884b654b975c3c
Colin McCullough <colin.n.mccullough@gmail.com>
2020-10-16 13:54:52 -0700

    ignore config test path

--- __tests__/scraper.spec.js
+++ __tests__/scraper.spec.js
@@ -1,5 +1,14 @@
 const Scraper = require('../scraper')
 const mockAxios = require('axios')
-const html = require('../config/html')
+const html = require('./config/html')
+const params = require('./config/params')
 
-describe('Scraper class', () => {})
+describe('Scraper class', () => {
+
+  const scraper = new Scraper(params)
+
+  test('constructor prop tests', () => {
+    expect(scraper.beforeScrape).toEqual([])
+
+  })
+})
