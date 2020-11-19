efd083c586b269d30a98ed716fcd0e7a70f5b2cf
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-11-19 14:44:35 -0800

    remove address function

--- scrapers/address.js
+++ scrapers/address.js
@@ -4,17 +4,26 @@ module.exports = {
   getAddress,
   parsedAddress
 }
-  function init(Scraper) {
-  Scraper.addProp('addressRegex', /\s*(\d+)((?:(?:\x20+[-'0-9A-zÀ-ÿ]+\.?)+?)\,?\x20*?)\-*,?\s+?\,?((?:[A-Za-z]+\x20*)+)\,\s(A[LKSZRAP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY])\s+(\d+(?:-\d+)?)*/gm, false)
-  Scraper.addProp('address', null, true)
+function init(Scraper) {
+  // const streetRegex = /^\d+\s[A-z]+\s[A-z]+/g
+  // Scraper.addProp('addressRegex', /\s*(\d+)((?:(?:\x20+[-'0-9A-zÀ-ÿ]+\.?)+?)\,?\x20*?)\-*,?\s+?\,?((?:[A-Za-z]+\x20*)+)\,\s(A[LKSZRAP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY])\s+(\d+(?:-\d+)?)*/gm, false)
+  // Scraper.addProp('address', null, true)
 
-  Scraper.addScraper('afterPageChange', getAddress)
+  // Scraper.addScraper('afterPageChange', getAddress)
 }
 
 function getAddress(scraper) {
-  const addresses =  scraper.$(scraper.template ? scraper.template.address.selector : 'body').text()
-  const matches = addresses.match(scraper.addressRegex)
-  scraper.address = parsedAddress(matches)
+  const addresses = scraper.$(scraper.template.address.selector ? scraper.template.address.selector : 'body').text()
+  const arrAddresses = addresses.replace(/\s\s+/gm, ' ')
+    .match(/.{0,50}\b\d{5}(?:-\d{4})?\b/g)
+  if (arrAddresses.length > 0) {
+    arrAddresses.forEach(address => {
+      const matches = address.match(scraper.addressRegex)
+      if (matches) {
+        scraper.address = parsedAddress(matches)
+      }
+    });
+  }
 }
 
 function parsedAddress(address) {
