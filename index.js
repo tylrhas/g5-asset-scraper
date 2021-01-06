afa680d5ca6568e40b994a4417bdcad41ee1f5f1
Tyler Hasenoehrl <tylrhas@gmail.com>
2021-01-06 14:36:38 -0800

    remove packages

--- index.js
+++ index.js
@@ -7,22 +7,14 @@ const Scraper = require('./scraper')
 const { upload } = require('./cloudinary')
 
 app.post('/', async (req, res) => {
+  const {body} = req
   try {
-    const { body } = req
-    console.log(body.topicName)
-    if (body.topicName) {
-      const scraper = new Scraper(body)
-      scraper.run()
-      // const results = scraper.results()
-      res.sendStatus(200) 
-    } else {
-      const scraper = new Scraper(body)
-      await scraper.run()
-      const results = scraper.results()
-      res.json(results) 
-    }
-
+    const scraper = new Scraper(body)
+    await scraper.run()
+    const results = scraper.results()
+    res.json(results)
   } catch (error) {
+    console.log(error)
     res.status(422).send(error.message)
   }
 })
