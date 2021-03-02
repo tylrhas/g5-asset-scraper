4db9304f85d6d8b2be3569466f555744c8282862
David Miller <david.miller@getg5.com>
2021-03-02 10:58:17 -0800

    update deprecated constructor

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 require('dotenv').config()
-const express = require('express')
-// const bodyParser = require('body-parser')
-const app = express({ limit: '1000kb' })
-// app.use(bodyParser.json())
-const Scraper = require('./scraper')
+import express from 'express'
+import {json } from 'body-parser'
+import Scraper from './scraper'
+
+const app = express()
+
+app.use(json({ limit: '1000kb' }))
+
+app.get('/', (req, res) => res.status(200).send('I\'m Listening.'))
 
 app.post('/', async (req, res) => {
   try {
     const { body } = req
-    console.log(req)
-    if (body.topicName) {
-      const scraper = new Scraper(body)
-      scraper.run()
-      res.sendStatus(200) 
-    } else {
-      const scraper = new Scraper(body)
-      await scraper.run()
-      const results = scraper.results()
-      res.json(results) 
-    }
+    console.log(req.body)
+    const scraper = new Scraper(body)
+    await scraper.run()
+    const results = scraper.results()
+    res.json(results) 
   } catch (error) {
     res.status(422).send(error.message)
   }
